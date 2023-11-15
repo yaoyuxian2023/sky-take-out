80eceac0b19ecd750381fb1c7ddb3e8de766ae8b
yaoyuxian2023 <13414061936@163.com>
2023-11-15 09:37:55 +0800

    init

---
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -0,0 +1,20 @@
+package com.sky.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Augus
+ */
+@Component
+@ConfigurationProperties(prefix = "sky.alioss")
+@Data
+public class AliOssProperties {
+
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String bucketName;
+
+}
