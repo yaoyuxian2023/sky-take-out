d57e730682da6d346b190cbe8e2ae289b31a02df
yaoyuxian2023 <13414061936@163.com>
2023-11-15 09:58:07 +0800

    init

---
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -0,0 +1,50 @@
+package com.sky.result;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 后端统一返回结果
+ * @author Augus
+ * @param <T>
+ */
+@Data
+public class Result<T> implements Serializable {
+
+    /**
+     * 编码：1成功，0和其它数字为失败
+     */
+    private Integer code;
+
+    /**
+     * 错误信息
+     */
+    private String msg;
+
+    /**
+     * 数据
+     */
+    private T data;
+
+    public static <T> Result<T> success() {
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(T object) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result result = new Result();
+        result.msg = msg;
+        result.code = 0;
+        return result;
+    }
+
+}
