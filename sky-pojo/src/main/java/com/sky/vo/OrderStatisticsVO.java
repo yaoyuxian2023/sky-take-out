7d5ef3d44ee8af18ce5ba6812702dd231c810a95
yaoyuxian2023 <13414061936@163.com>
2023-11-15 09:56:33 +0800

    init

---
+++ sky-pojo/src/main/java/com/sky/vo/OrderStatisticsVO.java
@@ -0,0 +1,19 @@
+package com.sky.vo;
+
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * @author Augus
+ */
+@Data
+public class OrderStatisticsVO implements Serializable {
+    //待接单数量
+    private Integer toBeConfirmed;
+
+    //待派送数量
+    private Integer confirmed;
+
+    //派送中数量
+    private Integer deliveryInProgress;
+}
