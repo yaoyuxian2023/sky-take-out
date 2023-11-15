202202384a70ff134e02bd6921d622eac0c7bdcc
yaoyuxian2023 <13414061936@163.com>
2023-11-15 09:53:01 +0800

    init

---
+++ sky-common/src/main/java/com/sky/exception/OrderBusinessException.java
@@ -0,0 +1,12 @@
+package com.sky.exception;
+
+/**
+ * @author Augus
+ */
+public class OrderBusinessException extends BaseException {
+
+    public OrderBusinessException(String msg) {
+        super(msg);
+    }
+
+}
