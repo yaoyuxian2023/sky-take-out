4c431f844d0a528d28bd686bb94bda771b2fff49
yaoyuxian2023 <13414061936@163.com>
2023-11-15 09:37:09 +0800

    init

---
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -0,0 +1,23 @@
+package com.sky.service;
+
+import com.sky.entity.AddressBook;
+import java.util.List;
+
+/**
+ * @author Augus
+ */
+public interface AddressBookService {
+
+    List<AddressBook> list(AddressBook addressBook);
+
+    void save(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void setDefault(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+}
