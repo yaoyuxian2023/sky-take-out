dcfd0737c336a76df481c5ae8370374902bc1982
yaoyuxian2023 <13414061936@163.com>
2023-11-15 09:38:23 +0800

    init

---
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -0,0 +1,22 @@
+package com.sky.context;
+
+/**
+ * @author Augus
+ */
+public class BaseContext {
+
+    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    public static Long getCurrentId() {
+        return threadLocal.get();
+    }
+
+    public static void removeCurrentId() {
+        threadLocal.remove();
+    }
+
+}
